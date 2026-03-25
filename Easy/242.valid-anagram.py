f3708d737eab832d6ba5a0b40ba4ca005b0959e8
2kiton <nicorodotorres@gmail.com>
2026-03-25 18:19:09 +1300

    got an answer, was wrong but will fix soon

--- Easy/242.valid-anagram.py
+++ Easy/242.valid-anagram.py
@@ -8,8 +8,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        if not (len(s) == len(t)):
-            return False
+        string1 = list(s)
+        string2 = list(t)
+        sum1 = 0
+        sum2 = 0 
+        isAnagram = False
         
+        for letter in string1:
+            sum1 += ord(letter)
+        
+        for letter in string2:
+            sum2 += ord(letter)
+            
+        if not (len(string1) == len(string2)):
+            isAnagram = False  
+        elif sum1 == sum2:
+            isAnagram = True
+        
+        return isAnagram
 # @lc code=end
 
